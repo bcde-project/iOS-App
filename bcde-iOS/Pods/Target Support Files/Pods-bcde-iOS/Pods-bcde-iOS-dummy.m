323ee47173932d46a60a947bc01d50c1362a3630
asd147asd147 <asd147asd147@naver.com>
2022-07-04 20:16:47 +0900

    init settings

---
+++ bcde-iOS/Pods/Target Support Files/Pods-bcde-iOS/Pods-bcde-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bcde_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_bcde_iOS
+@end
---
+++ bcde-iOS/bcde-iOS/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  bcde-iOS
+//
+//  Created by 최원준 on 2022/07/04.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
